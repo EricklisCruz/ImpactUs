816b9493ae56d49482839646411921e5be052326
EricklisCruz <erickliscruz@gmail.com>
2023-09-07 15:33:32 -0300

    created project "Impact Us"

---
+++ src/main/java/com/impactus/Impactus/ImpactusApplication.java
@@ -0,0 +1,13 @@
+package com.impactus.Impactus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpactusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpactusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/impactus/Impactus/ImpactusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impactus.Impactus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpactusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
